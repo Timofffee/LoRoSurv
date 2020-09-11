f05763a570b4d366899a20582ce2e1c4179e30dd
George Linkovsky <krougz@live.ru>
2020-09-11 18:54:43 +0400

    append tilemap and improve player controller

--- resources/hero/player.gd
+++ resources/hero/player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
 const SPEED = 16 #px/sec
+const RUN_SPEED = 32 
 
 var dir = Vector2.ZERO
 var last_dir = Vector2.RIGHT
+var running = false
 
 onready var anim_player = $anim
 
@@ -13,6 +15,9 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	
+	running = Input.is_action_pressed('run')
+	
 	dir = Vector2.ZERO
 	if Input.is_action_pressed('move_up'):
 		dir.y -= 1
@@ -24,24 +29,25 @@ func _process(delta: float) -> void:
 		dir.x += 1
 	
 	if dir != Vector2.ZERO:
-		move_and_slide(dir.normalized()*SPEED)
-		last_dir = dir
+		last_dir = move_and_slide(dir.normalized() * (RUN_SPEED if running else SPEED)).normalized()
+		
+		
 	
 	update_anim()
 
 
 func update_anim() -> void:
-	var d = dir if dir != Vector2.ZERO else last_dir
+	var d = last_dir
 	var prefix = "walk_" if dir != Vector2.ZERO else "idle_"
 	var anim_dir = "right"
 	
-	if d.y == -1: #up
+	if d.y < 0: #up
 		anim_dir = "up"
-	elif d.y == 1: #down
+	elif d.y > 0: #down
 		anim_dir = "down"
-	elif d.x == 1: #right
+	elif d.x > 0: #right
 		anim_dir = "right"
-	elif d.x == -1: #left
+	elif d.x < 0: #left
 		anim_dir = "left"
 	
 	
@@ -49,6 +55,7 @@ func update_anim() -> void:
 
 
 func set_anim(anim_name):
+	anim_player.playback_speed = 0.75 if running else 0.3
 	if anim_player.current_animation != anim_name:
 		anim_player.play(anim_name)
 
