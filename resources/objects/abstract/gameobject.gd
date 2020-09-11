52e75fca9dd25c0d8adcc406c0cd9fdba18e9c43
LMC_dd <dds9435@gmail.com>
2020-09-12 01:18:13 +0300

    add logic for abstract class gameobject and resourceobject

--- resources/objects/abstract/gameobject.gd
+++ resources/objects/abstract/gameobject.gd
@@ -1,5 +1,25 @@
 extends KinematicBody2D
 class_name GameObject
 
+signal hp_changed(val)
+signal destroyed
+
+export(int) var hp = get_default_hp() setget set_hp
+export(bool) var delete_on_destruction = true
+
+
+func get_default_hp():
+	return 100
+
+
+func set_hp(val):
+	hp = val
+	emit_signal("hp_changed", val)
+	if hp <= 0:
+		emit_signal("destroyed", val)
+		if delete_on_destruction:
+			queue_free()
+
+
 func _ready():
-	pass # Replace with function body.
+	pass
---
+++ resources/objects/tree.gd
@@ -0,0 +1,7 @@
+extends ResourceObject
+class_name Wood
+
+
+func _ready():
+	pass # Replace with function body.
+
